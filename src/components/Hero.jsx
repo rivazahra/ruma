3bffbe1333c72c99080485ee1dc5cb756fda4222
rivazahra <riva.zahra88@gmail.com>
2024-12-31 10:58:38 +0700

    change hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,44 +1,36 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
-import hero1 from '../assets/hero1.webp';
-import hero2 from '../assets/hero2.webp';
-import hero3 from '../assets/hero3.webp';
-import hero4 from '../assets/hero4.webp';
+import hero1 from '../assets/hero1.webp'
+import hero2 from '../assets/hero2.webp'
+import hero3 from '../assets/hero3.webp'
+import hero4 from '../assets/hero4.webp'
 
-const carouselImages = [hero1, hero2, hero3, hero4];
+const carouselImages = [hero1, hero2, hero3, hero4]
 const Hero = () => {
   return (
-    <div className=' grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+    <div className=" grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
       <div>
-        <h1 className='max-w-2xl text-4xl font-bold tracking-tight  sm:text-6xl '>
-          We’re changing the way people shop.
-        </h1>
+        <h1 className="max-w-2xl text-4xl font-bold tracking-tight  sm:text-6xl ">A New Era of Furniture Shopping</h1>
 
-        <p className='mt-8 max-w-xl text-lg leading-8'>
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua. Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-          qui lorem cupidatat commodo.
+        <p className="mt-8 max-w-xl text-lg leading-8">
+          Discover a new way to shop for furniture—where style meets comfort and innovation. Explore thoughtfully designed pieces crafted to elevate your space and bring your vision to life. Let us redefine how you create your dream home.
         </p>
-        <div className='mt-10 '>
-          <Link to='products' className='btn btn-primary '>
+        <div className="mt-10 ">
+          <Link to="products" className="btn btn-primary ">
             Our Products
           </Link>
         </div>
       </div>
-      <div className='hidden  h-[28rem] lg:carousel carousel-center   p-4 space-x-4 bg-neutral rounded-box'>
-        {carouselImages.map((image, index) => {
+      <div className="hidden  h-[28rem] lg:carousel carousel-center   p-4 space-x-4 bg-neutral rounded-box">
+        {carouselImages.map((image) => {
           return (
-            <div key={image} className='carousel-item'>
-              <img
-                src={image}
-                className='rounded-box h-full w-80  object-cover'
-              />
+            <div key={image} className="carousel-item">
+              <img src={image} className="rounded-box h-full w-80  object-cover" />
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
-export default Hero;
+  )
+}
+export default Hero
