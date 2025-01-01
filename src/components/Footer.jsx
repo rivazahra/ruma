3289cf283b9c559f35a3fff00a1e40ce09633335
rivazahra <riva.zahra88@gmail.com>
2025-01-01 21:31:05 +0700

    Add favicon and footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import logo from '../assets/logo.svg'
+
+const Footer = () => {
+  return (
+    <footer className="footer bg-base-200 text-base-content p-10">
+    <aside>
+   <img src={logo} alt="" width={90} />
+      <p>
+        Ruma Industries Ltd.
+        <br />
+        Providing reliable tech since 1992
+      </p>
+    </aside>
+    <nav>
+      <h6 className="footer-title">Services</h6>
+      <a className="">Branding</a>
+      <a className="">Design</a>
+      <a className="">Marketing</a>
+      <a className="">Advertisement</a>
+    </nav>
+    <nav>
+      <h6 className="footer-title">Company</h6>
+      <a className="link link-hover" href='/about'>About us</a>
+      <a className="">Contact</a>
+      <a className="">Jobs</a>
+      <a className="">Press kit</a>
+    </nav>
+    <nav>
+      <h6 className="footer-title">Legal</h6>
+      <a className="">Terms of use</a>
+      <a className="">Privacy policy</a>
+      <a className="">Cookie policy</a>
+    </nav>
+  </footer>
+  )
+}
+
+export default Footer
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import { Outlet, useNavigation } from 'react-router-dom'
 import Header from '../components/Header'
 import Navbar from '../components/Navbar'
 import Loading from '../components/Loading'
+import Footer from '../components/Footer'
 
 const HomeLayout = () => {
   const nav = useNavigation()
@@ -13,12 +13,11 @@ const HomeLayout = () => {
         <Navbar/>
       {loading ? (
         <Loading/>) : (
-        
-
       <section className='align-element py-20'>
       <Outlet />
       </section>
       )}
+      <Footer/>
     </>
   )
 }
